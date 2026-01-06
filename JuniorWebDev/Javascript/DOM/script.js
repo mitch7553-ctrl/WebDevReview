64b8770d6ecf7f4dd6fbd82aa143ae72d3b9af27
Mitchell Brown <mitch7553@gmail.com>
2026-01-07 01:41:27 +0800

    Commiting changes

--- JuniorWebDev/Javascript/DOM/script.js
+++ JuniorWebDev/Javascript/DOM/script.js
@@ -2,21 +2,30 @@ var button = document.getElementById("enter");
 var input = document.getElementById('userinput');
 var ul = document.querySelector("ul");
 
+function inputLength () {
+	return input.value.length;
+}
 
-button.addEventListener("click", function() {
-	if (input.value.length > 0) {
+
+function createListElement() {
+		console.log(event);
 		var li = document.createElement("li");
 	    li.appendChild(document.createTextNode(input.value));
 	    ul.appendChild(li);
 	    input.value =  "";
-	}
-})
+}
 
-input.addEventListener("keypdown", function() {
-	if (input.value.length > 0) {
-		var li = document.createElement("li");
-	    li.appendChild(document.createTextNode(input.value));
-	    ul.appendChild(li);
-	    input.value =  "";
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
 	}
-})
+}
+
+function addListAfterKeyDown(event) {
+	if (inputLength() > 0 && event.key == "Enter") {
+		createListElement();
+	}
+}
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keydown", addListAfterKeyDown);
