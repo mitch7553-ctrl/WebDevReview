ad3c182f0ef3533a7bdfd5b0ac9b138e55e3dd6e
mitch7553-ctrl <mitch7553@gmail.com>
2026-01-07 21:32:56 +0800

    Working on Advance Javascript

---
+++ JuniorWebDev/AdvanceJavascript/advancecontolflow.js
@@ -0,0 +1,8 @@
+// condition ? exp1 : exp2;
+
+function isUserValid(bool) {
+	return bool
+}
+
+
+var answer = isUserValid(true) ？ "you may enter" : "Access denied";
--- JuniorWebDev/AdvanceJavascript/script.js
+++ JuniorWebDev/AdvanceJavascript/script.js
@@ -1,23 +1,29 @@
 //.scope 
 
-var b = " Can I access this ?";
-
-function bb() {
-	var b = "hello";
-}
-
-
-
 //root scope 
 var fun = 5;
 
 function funFunction() {
+	//child scope 
 	var fun = "hello";
-	console.log(fun);
+	console.log(1,fun);
 }
-
-
 
 function funerFunction() {
-	
+	//child scope
+	var fun = "bye";
+	console.log(2, fun);
 }
+
+
+function funestFunction() {
+	//child scope
+	var fun = "Ahhhhh";
+	console.log(3, fun);
+}
+
+
+console.log("window", fun);
+funFunction();
+funerFunction();
+funestFunction();
