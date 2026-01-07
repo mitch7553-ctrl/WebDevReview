80a66d3d8dc0765cf2f751af459bc001ab950d9e
mitch7553-ctrl <mitch7553@gmail.com>
2026-01-07 20:30:46 +0800

    Working through Javascript Advance section

---
+++ JuniorWebDev/AdvanceJavascript/script.js
@@ -0,0 +1,23 @@
+//.scope 
+
+var b = " Can I access this ?";
+
+function bb() {
+	var b = "hello";
+}
+
+
+
+//root scope 
+var fun = 5;
+
+function funFunction() {
+	var fun = "hello";
+	console.log(fun);
+}
+
+
+
+function funerFunction() {
+	
+}
