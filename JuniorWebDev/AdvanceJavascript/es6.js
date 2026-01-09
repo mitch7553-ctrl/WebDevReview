fdd445e8eff059d4f2b31f660201cee388fe4669
Mitchell Brown <mitch7553@gmail.com>
2026-01-09 09:24:09 +0800

    finished the newer syntax

---
+++ JuniorWebDev/AdvanceJavascript/es6.js
@@ -0,0 +1,19 @@
+//ECMA internation 
+//ECMAS == Javascript 
+
+// let + const 
+
+// const player = 'bobby';
+// let experience = 100;
+// let wizardLevel = false;
+
+// if (experience > 90 ) {
+// 	let wizardLevel = true;
+// 	console.log('inside',wizardLevel);
+// }
+
+
+// console.log('outside', wizardLevel);
+
+// const a = function() { 
+// }
