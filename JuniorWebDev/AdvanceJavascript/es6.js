ee2e55f8dc6d3cf9c43c050fd67f39eefc20848c
Mitchell Brown <mitch7553@gmail.com>
2026-01-09 09:25:03 +0800

    forgot to save

--- JuniorWebDev/AdvanceJavascript/es6.js
+++ JuniorWebDev/AdvanceJavascript/es6.js
@@ -1,19 +1,67 @@
-//ECMA internation 
-//ECMAS == Javascript 
+// //ECMA internation 
+// //ECMAS == Javascript 
 
-// let + const 
+// // let + const 
 
-// const player = 'bobby';
-// let experience = 100;
-// let wizardLevel = false;
+// // const player = 'bobby';
+// // let experience = 100;
+// // let wizardLevel = false;
 
-// if (experience > 90 ) {
-// 	let wizardLevel = true;
-// 	console.log('inside',wizardLevel);
+// // if (experience > 90 ) {
+// // 	let wizardLevel = true;
+// // 	console.log('inside',wizardLevel);
+// // }
+
+
+// // console.log('outside', wizardLevel);
+
+// // const a = function() { 
+// // }
+
+// //Template strings 
+
+// // const name = "Sally";
+// // const age = 34;
+// // const pet = "horse";
+
+// // const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have! `;
+
+// // //Destructuring
+// // const  name = 'john snow';
+
+// // const obj =  { [name]: 'hello'
+// // [ray]: 'hihi'}
+
+// // const {player, experience}  = obj; 
+
+// // let { wizardLevel}  = obj;
+
+
+
+
+// // //default argument 
+// // function greet(name= '', age=30, pet='cat') {
+// // 	return greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have! `;
+// // }
+
+
+
+// // // Symbol 
+
+// // var sym1 = Symbol();
+// // var sym2 = Symbol('foo');
+// // var sym3 = ('foo');
+
+
+
+// // arrow functions 
+
+
+
+
+// function add(a, b) {
+// 	return a+b
 // }
 
 
-// console.log('outside', wizardLevel);
-
-// const a = function() { 
-// }
+// const add2 = (a,b) => a + b;
