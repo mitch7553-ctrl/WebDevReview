780342481f07ab18408988bd54caa4eeebbd30d2
Mitchell Brown <mitch7553@gmail.com>
2026-01-14 01:05:45 +0800

    working through the rest of advance javascript

--- JuniorWebDev/AdvanceJavascript/advanceObjects.js
+++ JuniorWebDev/AdvanceJavascript/advanceObjects.js
@@ -4,5 +4,36 @@
 var object1 = {value: 10};
 var object2 = object1;
 var object3 = {value: 10};
+
+
 //context 
+const object4 = {
+	a: function() {
+		console.log(this);
+	}
+}
+
+
 //instantiation
+class Player  {
+	constructor(name, type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	introduce() {
+		console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+	}
+}
+
+class Wizard extends Player {
+	constructor(name, type)  {
+		super(name, type) 
+	} 
+	play() { 
+		console.log(`WEEEEE I'm a ${this.type}`);
+	}
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic');
