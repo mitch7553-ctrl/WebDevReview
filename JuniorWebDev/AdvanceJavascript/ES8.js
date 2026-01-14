b92b0addf081ff9a8f85d4dc1980e3506266a1f5
Mitchell Brown <mitch7553@gmail.com>
2026-01-14 21:41:50 +0800

    Almost finish with advance javascript

---
+++ JuniorWebDev/AdvanceJavascript/ES8.js
@@ -0,0 +1,47 @@
+// //ES8 
+// .padStart(0)
+// .padEnd()
+
+
+const fun =(
+	a,
+	b,
+	,c
+	,d
+	) => {
+	console.log(a);
+}
+
+
+fun(1,2,3,4)
+
+
+Object.values 
+Object.entries
+Object.keys
+
+let obj  = {
+	username0: 'Santa',
+	username1: 'Rudolf',
+	username2: 'Mr. Grinch'
+}
+
+
+Object.keys(obj).forEach((key, index) =>{
+	console.log(key, obj[key]);
+})
+
+
+
+Object.keys(obj).forEach( value =>{
+	console.log(value);
+})
+
+Object.entries(obj).forEach(value => {
+	console.log(value);
+})
+
+
+Object.entries(obj).map(value => {
+	return value[1] + value[0].replace('username', '');
+} )
